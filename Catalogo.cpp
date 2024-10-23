8ec069092e3b59e7e46d51523434bab157ada6a0
BarbieMunoz <a01659348@tec.mx>
2024-10-22 20:26:44 -0600

    Removed the couts errors

--- Catalogo.cpp
+++ Catalogo.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 Catalogo::Catalogo() {
     sizeCatalogo = 0;
-    // semilla para la función random
+    // semilla para la funciÃ³n random
     srand(time(0));
 }
 
@@ -18,7 +18,6 @@ bool Catalogo::cargarCsv(const string& fileName) {
     // abre el archivo
 	ifstream file(fileName);
     if (!file.is_open()) {
-        cerr << "Error, the file can't be opened." << endl;
         return false;
     }
 
@@ -29,25 +28,24 @@ bool Catalogo::cargarCsv(const string& fileName) {
 
     // se empieza un loop while para que lea todas las lineas y las agregue al arbol binario
     while (getline(file, celda)) {
-        // ayuda a la extracción de datos como string y separa los valores por comas en este caso
+        // ayuda a la extracciÃ³n de datos como string y separa los valores por comas en este caso
         stringstream ss(celda);
 
-        // declaración de variables a usar, todas se establecen primero como str para despues hacer validación
+        // declaraciÃ³n de variables a usar, todas se establecen primero como str para despues hacer validaciÃ³n
         string name, type, size, align, crString, acString, hpString, error;
         double cr;
         int ac, hp;
 
         getline(ss, name, ',');
-        // si está vació el input se salta a la siguiente fila y se omite esta fila del mosntruo;
+        // si estÃ¡ vaciÃ³ el input se salta a la siguiente fila y se omite esta fila del mosntruo;
         if (isEmpty(name)) {
             continue;
         }
 
         getline(ss, crString, ',');
         if (!isEmpty(crString)) {
-            // se verifica si es válido
+            // se verifica si es vÃ¡lido
             if (!validDouble(crString)) {
-                cerr << "Error, the input is not a double." << endl;
                 continue;
             }
         }
@@ -67,9 +65,8 @@ bool Catalogo::cargarCsv(const string& fileName) {
 
         getline(ss, acString, ',');
         if (!isEmpty(acString)) {
-            // se verifica si es válido
+            // se verifica si es vÃ¡lido
             if (!validInt(acString)) {
-                cerr << "Error, the input is not an int." << endl;
                 continue;
             }
         }
@@ -79,9 +76,8 @@ bool Catalogo::cargarCsv(const string& fileName) {
 
         getline(ss, hpString, ',');
         if (!isEmpty(hpString)) {
-            // se verifica si es válido y se convierte a int
+            // se verifica si es vÃ¡lido y se convierte a int
             if (!validInt(hpString)) {
-                cerr << "Error, the input is not an int." << endl;
                 continue;
             }
         }
@@ -94,9 +90,8 @@ bool Catalogo::cargarCsv(const string& fileName) {
             continue;
         }
 
-        // si se leyó otra celda, se pasa a la siguiente fila
+        // si se leyÃ³ otra celda, se pasa a la siguiente fila
         if (getline(ss, error, ',')) {
-            cerr << "There´s an error in the catalog. Excessive number of cells. " << endl;
             continue;
         }
 
@@ -108,7 +103,7 @@ bool Catalogo::cargarCsv(const string& fileName) {
         // se crea el objeto de monstruo con lo leido de csv
         Monstruo nuevoMonstruo(name, cr, type, size, ac, hp, align);
 
-        // se añade al arbol usando las sobrecargas de operadores de clase Monstruo y compara su nombre en orden
+        // se aÃ±ade al arbol usando las sobrecargas de operadores de clase Monstruo y compara su nombre en orden
         arbolMonstruo.insertar(nuevoMonstruo);
         sizeCatalogo++;
     }
@@ -117,19 +112,18 @@ bool Catalogo::cargarCsv(const string& fileName) {
     return true;
 }
 
-// función para ver si las celdas estaban vacías o no
+// funciÃ³n para ver si las celdas estaban vacÃ­as o no
 bool Catalogo::isEmpty(const string& variable) {
     if (variable.empty()) {
-        cerr << "There´s an error in the catalog. Insufficient number of cells." << endl;
         return true;
     }
     return false;
 }
 
-// función para verificar si el dato es un entero
+// funciÃ³n para verificar si el dato es un entero
 bool Catalogo::validInt(const string& variable) {
     for (int i = 0; i < variable.length(); i++) {
-        // si en la posición de la variable string no es un número, es incorrecto el valor
+        // si en la posiciÃ³n de la variable string no es un nÃºmero, es incorrecto el valor
         if (!isdigit(variable[i])) {
             return false;
         }
@@ -137,20 +131,20 @@ bool Catalogo::validInt(const string& variable) {
     return true;
 }
 
-// función para verificar si el dato es un double
+// funciÃ³n para verificar si el dato es un double
 bool Catalogo::validDouble(const string& variable) {
     bool puntoDecimal = false;
     for (int i = 0; i < variable.length(); i++) {
-        // condición de cuando ya se había visto un punto decimal en la variable
+        // condiciÃ³n de cuando ya se habÃ­a visto un punto decimal en la variable
         if (variable[i] == '.') {
-            // si puntoDecial = true significa que la variable no es double pues tiene más de 1 punto
+            // si puntoDecial = true significa que la variable no es double pues tiene mÃ¡s de 1 punto
             if (puntoDecimal) {
                 return false;
             }
 
-            // si pasa no había un punto decimal, ahora puntoDecimal se vuelve true para decir que ya hay un punto en la vaiable
+            // si pasa no habÃ­a un punto decimal, ahora puntoDecimal se vuelve true para decir que ya hay un punto en la vaiable
             puntoDecimal = true;
-            // pasa a la siguiente iteración
+            // pasa a la siguiente iteraciÃ³n
             continue;
         }
         if (!isdigit(variable[i])) {
@@ -165,7 +159,7 @@ int Catalogo::getSize() {
 }
 
 Monstruo* Catalogo::randomMonster() {
-    // condición de que no hay monstruos
+    // condiciÃ³n de que no hay monstruos
     if (sizeCatalogo == 0) {
         return nullptr;
     }
@@ -175,7 +169,7 @@ Monstruo* Catalogo::randomMonster() {
     
     NodoArbol<Monstruo>* monstruo = arbolMonstruo.recorrerArbol(valorAleatorio);
     if (monstruo) {
-        //  sin el ->dato, estaría regresando el nodo y con & devuelve la dirección del monstruo
+        //  sin el ->dato, estarÃ­a regresando el nodo y con & devuelve la direcciÃ³n del monstruo
         return &monstruo->dato;
     }
 
