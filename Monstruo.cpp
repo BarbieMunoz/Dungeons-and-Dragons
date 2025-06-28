94dee257d444dc0f3556871592a59c236d417846
BarbieMunoz <a01659348@tec.mx>
2025-06-28 16:14:20 -0600

    changed some functions to support the new functionality

--- Monstruo.cpp
+++ Monstruo.cpp
@@ -30,11 +30,13 @@ int Monstruo::getAc() {
 	return ac;
 }
 
-// se usa función miembro porque solo necesita un parametro (en este caso el otro monstruo a comprar)
+string Monstruo::getName() {
+	return name;
+}
+
+// se usa funci�n miembro porque solo necesita un parametro (en este caso el otro monstruo a comprar)
 bool Monstruo::operator<(const Monstruo& monstruoLeft) const {
-	// cambia la condición para que se verifique el nombre de los monstruos al insertarlos al arbol
-	// seleccioné el nombre porque los parámetros como cr o ar tienen valores iguales
-	// y mi arbol solo acepta insertar nodos diferentes
+	// cambia la condici�n para que se verifique el nombre de los monstruos al insertarlos
 	return this->name < monstruoLeft.name;
 }
 
@@ -43,7 +45,17 @@ bool Monstruo::operator>(const Monstruo& monstruoRight) const {
 	return this->name > monstruoRight.name;
 }
 
-// no es Monstruo:: porque es función amiga por lo que es como una función global
+bool Monstruo::operator==(const Monstruo& monstruo) const {
+	// true si son iguales
+	return  name == monstruo.name;
+}
+
+bool Monstruo::operator!=(const Monstruo& monstruo) const {
+	// se niega la sobrecarga de ==
+	return !(*this == monstruo);
+}
+
+// no es Monstruo:: porque es funci�n amiga por lo que es como una funci�n global
 ostream& operator<<(ostream& os, const Monstruo& monstruo) {
 	os << "Name: " << monstruo.name
 		<< "\nChallenge rating: " << monstruo.cr
--- Monstruo.h
+++ Monstruo.h
@@ -20,10 +20,13 @@ public:
 	~Monstruo();
 
 	int getAc(void);
+	string getName(void);
 
-	// sobrecarga de operadores para poder comparar en "insertar" del arbol
+	// sobrecarga de operadores para poder comparar
 	bool operator<(const Monstruo& monstruoLeft) const;
 	bool operator>(const Monstruo& monstruoRight) const;
+	bool operator==(const Monstruo& monstruo) const;
+	bool operator!=(const Monstruo& monstruo) const;
 
 	// funciones friend son funciones externas que tienen acceso a miembros privados
 	friend ostream& operator<<(ostream& os, const Monstruo& monster);
